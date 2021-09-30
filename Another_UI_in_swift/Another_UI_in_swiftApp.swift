71e07bcef477c7a788399738b35a3139381f9452
Maksim Tochilkin <mtoch777@gmail.com>
2021-09-30 11:42:48 -0400

    Initial Commit

---
+++ Another_UI_in_swift/Another_UI_in_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Another_UI_in_swiftApp.swift
+//  Another_UI_in_swift
+//
+//  Created by Maksim Tochilkin on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Another_UI_in_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Another_UI_in_swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Another_UI_in_swift
+//
+//  Created by Maksim Tochilkin on 9/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
